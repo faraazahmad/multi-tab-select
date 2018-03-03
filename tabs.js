e7cae5f55c8dbbbfb017b33436f8ff1727966f7a
Syed Faraaz Ahmad <faraaz98@live.com>
2018-03-03 19:36:53 +0530

    sort out eventlisteners

--- tabs.js
+++ tabs.js
@@ -1,26 +1,30 @@
-function untoggleClassOnAllTabs() {
+TabList = document.querySelectorAll('.tab');
+
+function selectOnlyCurrentTab(targetTab) {
     TabList.forEach((tab) => {
-        tab.classList.remove('current')
+        tab.classList.remove('current');
     })
+    targetTab.classList.add('current');
 }
 
-TabList = document.querySelectorAll('.tab');
+function toggleCurrentTab(targetTab) {
+    targetTab.classList.toggle('current');
+}
 
 document.addEventListener("keydown", (key) => {
     if (key.keyCode == 17) {
         TabList.forEach((tab) => {
-            tab.addEventListener("click", () => {
-                // untoggleClassOnAllTabs();
-                tab.classList.add('current');
-            })
-        }) 
-    }
-    else {
-        TabList.forEach((tab) => {
-            tab.addEventListener("click", () => {
-                untoggleClassOnAllTabs();
-                tab.classList.add('current');
-            })
+            tab.removeEventListener("click", selectOnlyCurrentTab(tab));
+            tab.addEventListener("click", toggleCurrentTab(tab));
         })
     }
-})
+});
+
+document.addEventListener("keyup", (key) => {
+    if (key.keyCode == 17) {
+        TabList.forEach((tab) => {
+            tab.removeEventListener("click", toggleCurrentTab(tab));
+            tab.addEventListener("click", selectOnlyCurrentTab(tab));
+        })
+    }
+});
